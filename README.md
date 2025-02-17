fdeee28db58f15acc71afb06c5a5c4abbff7c08e
this-scott <this_scott@outlook.com>
2025-02-17 01:23:19 -0500

    Dist code finished. Need to dockerize with tailscale included then

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Cookie
+# BD
 Background: I a conversation with a recruiter who said I didn't have a lot of backend experience. He is right so I wanted to get some practice.
 
 Not sure what this is going to be right now. I'm thinking about turning it into a blog site.
@@ -7,6 +7,6 @@ My degree focuses on security and networking and much of my experience at my job
 
 Also Pitt doesn't have a lot of web dev classes besides 1520 and Cloud Computing so I need to get practice with backend development on my own.
 
-I hope I have time to work on this
+Kind of liking how this is turning out so far
 
 ![BD](dist/static/BD.png)
--- dist/dist.go
+++ dist/dist.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"html/template"
 	"io"
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 
 	"github.com/joho/godotenv"
 )
@@ -26,6 +28,7 @@ func basicHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 // Accept new content
+// ngl if the content server(literally exists just to serve) is just going to be my laptop I should store old versions in the log server
 func contentHandler(w http.ResponseWriter, r *http.Request) {
 	// A webhook is just an api which sends nothing back
 	if r.Method != http.MethodPut {
@@ -35,33 +38,54 @@ func contentHandler(w http.ResponseWriter, r *http.Request) {
 
 	// validating. There is definitely a better way to validate this
 	if r.Header.Get("Key") == os.Getenv("authkey") {
+
+		//R storing the files within itself as r.MultipartForm instead of returning as an array is evil and akward
 		err := r.ParseMultipartForm(10 << 20) //allocates 10mb file size
 		if err != nil {
 			//using this line because we're not returning anything to content server
 			fmt.Println(err.Error())
 			return
 		}
-
-		//write new file(s)
-		for fname := range r.MultipartForm.File {
-			file, header, err := r.FormFile(fname)
-			if err != nil {
-				fmt.Println(err)
-				//fmt.Errorf("Error retrieving file %d", http.StatusBadRequest)
-				return
-			}
-			defer file.Close()
-
-			outFile, err := os.Create("./static/" + header.Filename)
+		//checking version. Perfectly ok with rollbacks
+		versionValue, exists := r.Form["sversion"]
+		if !exists || len(versionValue) == 0 {
+			fmt.Println("Version not found")
+			return
+		} else {
+			//serveVersion here isn't the global var above
+			serveVersion, err := strconv.Atoi(versionValue[0])
 			if err != nil {
 				fmt.Println(err.Error())
-				//fmt.Errorf("Error saving file %d", http.StatusInternalServerError)
 				return
 			}
-			defer outFile.Close()
+			fmt.Println("Changing to Version:", serveVersion)
+		}
 
-			io.Copy(outFile, file)
-			fmt.Printf("Uploaded: %s\n", header.Filename)
+		fmt.Println(r.MultipartForm.File)
+		//write new file(s)
+		for f, headers := range r.MultipartForm.File {
+			fmt.Println(f)
+			for _, header := range headers {
+				file, err := header.Open() // Open each file
+				if err != nil {
+					fmt.Println("Error opening file:", err)
+					continue
+				}
+				defer file.Close()
+
+				//create a blank copy with the same name as the file
+				outFile, err := os.Create("./static/" + header.Filename)
+				if err != nil {
+					fmt.Println(err.Error())
+					//fmt.Errorf("Error saving file %d", http.StatusInternalServerError)
+					return
+				}
+				defer outFile.Close()
+
+				//copy file contents in
+				io.Copy(outFile, file)
+				fmt.Printf("Uploaded: %s\n", header.Filename)
+			}
 		}
 	} else {
 		err := fmt.Errorf("bad key in content handler: %s", r.Header.Get("Key"))
@@ -70,26 +94,37 @@ func contentHandler(w http.ResponseWriter, r *http.Request) {
 
 }
 
-// ship to log server
-func sendToLog() {
+func init() {
+	//LMAO I DIDN'T REALIZE INIT IS CALLED AUTOMATICALLY I WAS GOING TO THROW IT IN FRONT OF MAIN
 
-}
-
-// func init() {
-// 	//check to see if distribution content is updated
-// }
-
-func main() {
-	//EVERYTHING BEFORE TODO SHOULD BE WRAPPED IN THE INIT FUNCTION BUT GO'S SCOPE DOESN'T WORK LIKE THAT
-
-	//TODO: SETUP FOR HTTPS
-	fmt.Println("Hello World")
 	err := godotenv.Load(".env")
 	//Something about this line brings me joy
 	if err != nil {
 		panic(err.Error())
 	}
 
+	//the internet says I should send my integers raw ;P
+	body := bytes.NewBufferString(strconv.Itoa(serveVersion))
+
+	//sends request to content server to check for updates -> updates will be sent to update url
+	//this is going to be a hook to because I don't see a point writing the above content handler unless I want to wrap the change content function(lowkey probably should)
+	requestURL := fmt.Sprintf("%s%d", os.Getenv("log_server"), 8080)
+	req, err := http.NewRequest(http.MethodGet, requestURL, body)
+	if err != nil {
+		fmt.Printf("client: could not create request: %s\n", err)
+		os.Exit(1)
+	}
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		fmt.Printf("client: error making http request: %s\n", err)
+		os.Exit(1)
+	}
+	defer res.Body.Close()
+}
+
+func main() {
+	//TODO: SETUP FOR HTTPS
+	fmt.Println("Hello World")
 	//need to handle specified pages but in that case I need an actual landing page
 	//scoop.com/livelylanding
 	http.HandleFunc("/", basicHandler)
